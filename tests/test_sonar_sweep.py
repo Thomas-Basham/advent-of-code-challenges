471a1fb3b15298185b0032c71d2fe7eb7e0148e5
Thomas Basham <Bashamtg@gmail.com>
2022-10-18 16:10:57 -0700

    added sweep test

--- tests/test_sonar_sweep.py
+++ tests/test_sonar_sweep.py
@@ -6,13 +6,37 @@ def test_linked_list_exists():
 
 
 def test_add_one():
-  LL = LinkedList()
+  linked_list = LinkedList()
 
-  LL.insert(test_case[0])
+  linked_list.insert(test_case[0])
 
+  actual = str(linked_list)
   expected = '{ 199 } -> NULL'
+  assert actual == expected
 
-  assert str(LL) == expected
+
+def test_add_test_case():
+  linked_list = LinkedList()
+
+  for value in reversed(test_case):
+    linked_list.insert(value)
+
+  actual = str(linked_list)
+  expected = '{ 199 } -> { 200 } -> { 208 } -> { 210 } -> { 200 } -> { 207 } -> { 240 } -> { 269 } -> { 260 } -> { 263 } -> NULL'
+  assert actual == expected
+
+
+def test_sweep():
+  linked_list = LinkedList()
+
+  for value in reversed(test_case):
+    linked_list.insert(value)
+
+  linked_list.sweep()
+
+  actual = linked_list.count
+  expected = 7
+  assert actual == expected
 
 
 test_case = [
